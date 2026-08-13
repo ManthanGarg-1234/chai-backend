33aa54b43a7357b4cf2053f6f2b090660d834f48
ManthanGarg-1234 <manthangarg2024cse@gmail.com>
2026-08-14 00:56:49 +0530

    mongodb connection learned

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+ 
+
+
+
+/*
+import express from 'express'
+const app = express()
+(async () => {
+  try {
+    mongoose.connect(`${process.env.MONGO_URI}`);
+    app.on("error", (error) =>{
+        console.log("ERR: ",error)
+        throw error
+    })
+    app.listem(process.env.PORT, () =>{
+        console.log(`App listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw err;
+  }
+})();
+*/
